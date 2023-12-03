47ef2234a69285d12296726bad2bd14a3195ce5f
Micah Hahn <micah.s.hahn@gmail.com>
2023-12-02 23:44:19 -0600

    Working Day1 solution

--- src/Day1.purs
+++ src/Day1.purs
@@ -1,10 +1,16 @@
 module Day1
   ( challenge1
+  , parseLine
   ) where
 
 import Prelude
+
 import Challenge (Challenge)
+import Data.Foldable (foldl, sum)
 import Data.Maybe (Maybe(..))
+import Data.String as String
+import Data.Int as Int
+import Data.String.CodeUnits (fromCharArray, toCharArray)
 
 challenge1 :: Challenge
 challenge1 =
@@ -15,6 +21,43 @@ challenge1 =
       , "treb7uchet"
       ]
   , exampleAnswer: "142"
-  , solver: identity
+  , solver: solution1
   , solution: Nothing
   }
+
+parseLine :: String -> Int
+parseLine line =
+  let
+    maybeResult =
+      foldl
+        ( \maybeAccum char ->
+            if char >= '0' && char <= '9' then
+              case maybeAccum of
+                Nothing ->
+                  Just { first: char, last: char }
+                Just accum ->
+                  Just (accum { last = char })
+            else maybeAccum
+        )
+        Nothing
+        (toCharArray line)
+  in
+    case maybeResult of
+      Nothing ->
+        0
+
+      Just { first, last } ->
+        case Int.fromString (fromCharArray [ first, last ]) of
+          Nothing ->
+            0
+
+          Just x ->
+            x
+
+solution1 :: String -> String
+solution1 input =
+  let
+    lines = String.split (String.Pattern "\n") input
+  in
+    sum (map parseLine lines)
+      # Int.toStringAs Int.decimal
