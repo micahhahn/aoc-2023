14d098d8017c610dced2273ab18b59ac622fd44e
Micah Hahn <micah.s.hahn@gmail.com>
2023-12-03 22:04:34 -0600

    Day2 challenge 2

--- src/Day2.purs
+++ src/Day2.purs
@@ -1,6 +1,6 @@
 module Day2
   ( challenge1
-  , x
+  , challenge2
   ) where
 
 import Prelude
@@ -93,7 +93,7 @@ game = do
 
 gameCheck :: Game -> Boolean
 gameCheck { sets } =
-  all (\set -> set.red <= 12 && set.green <= 13 && set.blue <= 14) sets
+  all (\s -> s.red <= 12 && s.green <= 13 && s.blue <= 14) sets
 
 solution1 :: String -> String
 solution1 input =
@@ -105,14 +105,64 @@ solution1 input =
         ( \line -> case runParser game line of
             Left _ ->
               Nothing
-            Right game ->
-              if gameCheck game then
-                Just game
+            Right g ->
+              if gameCheck g then
+                Just g
               else
                 Nothing
         )
         lines
   in
-    map (\game -> game.gameId) possibleGames
+    map (\g -> g.gameId) possibleGames
+      # sum
+      # Int.toStringAs Int.decimal
+
+challenge2 :: Challenge
+challenge2 =
+  { examplePrompt:
+      [ "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+      , "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+      , "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+      , "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+      , "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+      ]
+  , exampleAnswer: "2286"
+  , solver: solution2
+  , promptPath: "assets/day2.txt"
+  , solution: Nothing
+  }
+
+gameMin :: Game -> Int
+gameMin game =
+  let
+    minSet =
+      foldl
+        ( \x s ->
+            { red: max x.red s.red
+            , green: max x.green s.green
+            , blue: max x.blue s.blue
+            }
+        )
+        { red: 0, green: 0, blue: 0 }
+        game.sets
+
+  in
+    minSet.red * minSet.green * minSet.blue
+
+solution2 :: String -> String
+solution2 input =
+  let
+    lines = String.split (String.Pattern "\n") input
+  in
+    map
+      ( \line ->
+          case runParser game line of
+            Left _ ->
+              0
+
+            Right g ->
+              gameMin g
+      )
+      lines
       # sum
       # Int.toStringAs Int.decimal
